bad6718377d0da100d199a9689af9a2d4c4242a7
Sam Frances <sam@samfrances.uk>
2021-06-12 08:03:30 +0100

    Trivial commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const favouriteFood = process.env.FAVOURITE_FOOD;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+  console.log(`My favourite food is ${favouriteFood}`);
   while(true) {
-    console.log('Containers rule!');
+    console.log('Containers rule!!');
     await sleep(5000);
   }
 }
